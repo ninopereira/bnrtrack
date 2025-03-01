206343ec783875b0c537bec5ad816e881f5a94d8
Nino Pereira <ninopereira.pt@gmail.com>
2025-03-01 12:20:28 +0000

    feat: tracking robot and displaying path with min and max area defined

--- track.py
+++ track.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 import time
 
+MIN_AREA = 50000
+MAX_AREA = 100000
+
 # Initialize video capture object
 cap = cv2.VideoCapture("sample.mp4")  # Use 0 for webcam
 
@@ -22,6 +25,11 @@ while True:
     # Apply the background subtractor
     fgmask = fgbg.apply(frame)
 
+    # Apply some noise reduction to the mask
+    kernel = np.ones((3, 3), np.uint8)
+    fgmask = cv2.erode(fgmask, kernel, iterations=1)
+    fgmask = cv2.dilate(fgmask, kernel, iterations=2)
+
     # Find contours
     contours, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -31,7 +39,10 @@ while True:
     # Find the largest contour (assuming it's the robot)
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area > 500 and area > largest_area:  # Adjust threshold as needed
+        print("area", area)
+        if (
+            area > MIN_AREA and area > largest_area and area < MAX_AREA
+        ):  # Adjust threshold as needed
             largest_area = area
             largest_contour = cnt
 
@@ -44,26 +55,24 @@ while True:
 
         points.append(center)
 
-        # Draw lines connecting the points
-        if len(points) > 1:
-            for i in range(1, len(points)):
-                cv2.line(
-                    display_frame, points[i - 1], points[i], (255, 0, 0), 5
-                )  # Green line, 2px thickness
-
         # Draw a blue dot at the current position
         cv2.circle(
             display_frame, center, 10, (255, 0, 0), -1
         )  # Blue dot, larger radius
 
+    # Draw lines connecting the points
+    if len(points) > 1:
+        for i in range(1, len(points)):
+            cv2.line(display_frame, points[i - 1], points[i], (255, 0, 0), 5)
     # Display the original frame with the blue dot
     cv2.imshow("Robot Tracking", display_frame)
+    cv2.imshow("Foreground Mask", fgmask)
 
     # Break the loop on 'q' key press
     if cv2.waitKey(30) & 0xFF == ord("q"):
         break
 
-    # time.sleep(0.05)
+    # time.sleep(0.2)
 
 # Release the video capture object and close all windows
 cap.release()
